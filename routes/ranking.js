91a200802a065bd1f8c65eb83692852b08494d3d
Noah Loomans <noahloomans@gmail.com>
2016-06-17 20:52:38 +0200

    fixed ; inconsistency

--- routes/ranking.js
+++ routes/ranking.js
@@ -5,72 +5,72 @@ let credly = require('../lib/credly')
 function resolveUserId(userId) {
   return new Promise(function(resolve, reject) {
     if (parseInt(userId) === NaN) {
-      resolve(parseInt(userId));
+      resolve(parseInt(userId))
     }
     else {
       credly.request(`/members/${userId}`).then(data => {
-        resolve(data.id);
+        resolve(data.id)
       })
     }
-  });
+  })
 }
 
 router.get('/:id/:badge', function(req, res, next) {
 
   resolveUserId(req.params.id).then(userId => {
-    let badgeTitle = req.params.badge;
+    let badgeTitle = req.params.badge
     credly.request(`/members/${userId}/badges/given`).then(badgesGiven => {
-      let resJson = [];
-      let users = [];
+      let resJson = []
+      let users = []
       badgesGiven
         .filter(badge => badge.title === badgeTitle)
         .forEach(badge => {
-          resJson.push(badge.member.display_name);
-          users.push(badge.member);
-        });
+          resJson.push(badge.member.display_name)
+          users.push(badge.member)
+        })
 
-      console.log(resJson);
-      console.log(users);
+      console.log(resJson)
+      console.log(users)
 
-      let apiPromises = [];
+      let apiPromises = []
 
       for (let i=0; i<users.length; i++) {
-        console.log(`pushing ${users[i].id}`);
-        apiPromises.push(credly.request(`/members/${users[i].id}/badges`));
+        console.log(`pushing ${users[i].id}`)
+        apiPromises.push(credly.request(`/members/${users[i].id}/badges`))
       }
 
-      console.log(apiPromises);
+      console.log(apiPromises)
 
       Promise.all(apiPromises).then(apiRes => {
-        let data = {users: []};
+        let data = {users: []}
         for (let userIndex=0; userIndex<apiRes.length; userIndex++) {
-          let name = users[userIndex].display_name;
-          let badges = apiRes[userIndex];
+          let name = users[userIndex].display_name
+          let badges = apiRes[userIndex]
           let ICTInDeWolkenBadges = badges.filter(badge => {
-            return badge.issuer.slug == 'ictindewolken';
-          });
-          data.users.push({name: name, numberOfCoderClassBadges: ICTInDeWolkenBadges.length});
+            return badge.issuer.slug == 'ictindewolken'
+          })
+          data.users.push({name: name, numberOfCoderClassBadges: ICTInDeWolkenBadges.length})
           console.log(`${name} has ${badges.length} badges, from \
-            with ${ICTInDeWolkenBadges.length} are ICT in de wolken badges`);
+            with ${ICTInDeWolkenBadges.length} are ICT in de wolken badges`)
         }
-        data.users.sort((a, b) => b.numberOfCoderClassBadges - a.numberOfCoderClassBadges);
-        res.render('ranking', data);
+        data.users.sort((a, b) => b.numberOfCoderClassBadges - a.numberOfCoderClassBadges)
+        res.render('ranking', data)
       })
     })
   })
 
 
   // let userId = await resolveUserId(req.params.id)
-  // let badgeTitle = req.params.badge;
+  // let badgeTitle = req.params.badge
   //
   // let badgesGiven = await credly.request(`/members/${userId}/badges/given`)
   // let resJson = []
   // badgesGiven
   //   .filter(badge => badge.title === badgeTitle)
   //   .forEach(badge => {
-  //     resJson.push(badge.member.display_name);
+  //     resJson.push(badge.member.display_name)
   //   })
-  // res.json(resJson);
+  // res.json(resJson)
 
 
 //   let users = req.params.id.split('+')
@@ -88,12 +88,12 @@ router.get('/:id/:badge', function(req, res, next) {
 //
 //   Promise.all(allApiPromises).then(apiRes => {
 //     for (let userIndex=0; userIndex<apiRes.length; userIndex++) {
-//       let name = apiRes[userIndex][0].display_name;
-//       let badges = apiRes[userIndex][1];
+//       let name = apiRes[userIndex][0].display_name
+//       let badges = apiRes[userIndex][1]
 //       let ICTInDeWolkenBadges = badges.filter(badge =>
 //         badge.issuer.slug == 'ictindewolken')
 //       console.log(`${name} has ${badges.length} badges, from \
-// with ${ICTInDeWolkenBadges.length} are ICT in de wolken badges`);
+// with ${ICTInDeWolkenBadges.length} are ICT in de wolken badges`)
 //     }
 //     res.json(apiRes)
 //   })
