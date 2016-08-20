0e22c448258c827aad9ecfb6e76898a51f79b67e
Noah Loomans <noahloomans@gmail.com>
2016-08-20 21:21:17 +0200

    refactoring

--- routes/ranking.js
+++ routes/ranking.js
@@ -2,76 +2,48 @@ let express = require('express')
 let router = express.Router()
 let credly = require('../lib/credly')
 
-// This allows us to use the es2016 await. for more info see
-// https://github.com/yortus/asyncawait
-let async = require('asyncawait/async')
-let await = require('asyncawait/await')
-
-/**
- * resolveUserId - resolves the user id or a slag in a credly user id.
- *
- * @param  {string or number} userId The user id or the slag of a credly user.
- * @return {number} the user id of a credy user.
- */
-function resolveUserId(userId) {
-  return new Promise(function(resolve, reject) {
-    if (parseInt(userId) === NaN) {
-      resolve(parseInt(userId))
-    }
-    else {
-      credly.request(`/members/${userId}`).then(data => {
-        resolve(data.id)
-      })
-    }
-  })
+function sortByLength (array) {
+  return array.sort((a, b) => b.length - a.length)
 }
 
 /**
- * GET /ranking/:id/:badge - generates a ranking table.
+ * requests all the badges of the members who where given the badges.
+ * @param  {Array} badges All the badges are requested for every user in the
+ *                        array.
+ * @return {Array}        Each item contains all of the users badges.
+ */
+function getBadgesOfMembers (badges) {
+  const promises = badges.map(badge =>
+    credly.request(`/members/${badge.member_id}/badges`))
+
+  return Promise.all(promises)
+}
+
+/**
+ * generates a ranking table.
  *
  * This router will look up witch users have a spesific badge, and rank them
  * based on how manny badges they got from {:id}.
  *
- * @param {:id} the userId or slag of the user who gave the identifying badge.
- * @param {:badge} the badge name used to identify the users to rank.
+ * @param {NumberOfString} id the userId or slag of the user who gave the
+ *                            identifying badge.
+ * @param {String} badge      the badge name used to identify the users to
+ *                            rank.
  */
-router.get('/:id/:badge', async (function(req, res, next) {
-
-  // The credly api only allows a user id, if a slag was given, resolve
-  // into a user id.
-  let identifyingUserId = await (resolveUserId(req.params.id))
-  let identifyingBadgeTitle = req.params.badge
-  let badgesGiven =
-      await (credly.request(`/members/${identifyingUserId}/badges/given`))
-  let resJson = []
-  let users = []
-  badgesGiven
-    .filter(badge => badge.title === identifyingBadgeTitle)
-    .forEach(badge => {
-      users.push(badge.member)
-    })
-
-  // We now have identifyed with users to rank, request the badges of these
-  // users.
-  let badgesOfMatchingUsersPromises =
-      users.map(user => credly.request(`/members/${user.id}/badges`))
-  let badgesOfMatchingUsers = await (badgesOfMatchingUsersPromises)
-
-  // Format the data.
-  let data = {users: []}
-  for (let userIndex=0; userIndex<badgesOfMatchingUsers.length; userIndex++) {
-    let userBadges = badgesOfMatchingUsers[userIndex]
-    data.users.push({
-      name: users[userIndex].display_name,
-      badges: userBadges,
-      badgesFromIdentifyingUser: userBadges.filter(badge =>
-          badge.issuer.id == identifyingUserId)
-    })
-  }
-  data.users.sort((a, b) =>
-      b.badgesFromIdentifyingUser.length - a.badgesFromIdentifyingUser.length)
-  res.render('ranking', data)
-
-}))
+router.get('/:id/:badge', function (req, res, next) {
+  credly.resolveUserId(req.params.id).then(issuerId => {
+    credly.request(`/members/${issuerId}/badges/given`)
+      .then(badges => badges.filter(badge => badge.title === req.params.badge))
+      .then(getBadgesOfMembers)
+      .then(badgesOfMembers => badgesOfMembers.map(
+        badges => badges.filter(badge => badge.issuer.id === issuerId)
+      ))
+      .then(sortByLength)
+      .then(badgesOfMembers => {
+        res.render('ranking', {badgesOfMembers})
+      })
+      .catch(console.error)
+  })
+})
 
 module.exports = router
--- routes/user.js
+++ routes/user.js
@@ -1,67 +1,35 @@
-let express = require('express')
-let router = express.Router()
-let credly = require('../lib/credly')
+const express = require('express')
+const router = express.Router()
+const credly = require('../lib/credly')
 
-router.get('/:id', function(req, res, next) {
+router.get('/:id', function (req, res, next) {
+  credly.getUserBadges(req.params.id)
+    .then(user => {
+      res.render('badges', {user})
+    })
+    .catch(err => {
+      res.render('error', {
+        message: 'Somthing went wrong...',
+        error: err
+      })
+    })
 
-  // let user = {}
-  //
-  // let apiPromises = [
-  //   credly.request(`/members/${req.params.id}`),
-  //   credly.request(`/members/${req.params.id}/badges`)
-  // ]
-  //
-  // console.time('api requests')
-  // Promise.all(apiPromises).then(apiRes => {
-  //   console.timeEnd('api requests')
-  //   user.name = apiRes[0].display_name
-  //   user.badges = apiRes[1].map(badge => {
-  //     return {
-  //       title: badge.title,
-  //       description: badge.description,
-  //       image: badge.image
-  //     }
-  //   })
-  //   console.log(user);
-  //   res.render('badges',{user: user})
+  // res.render('badges', {user:
+  // { name: 'Noah Loomans', // 2193255
+  //   badges:
+  //    [ { title: 'SQL-0',
+  //        description: 'De inhoud van de badge vind je onder\r\n\r\nhttp://infvo.com/ictindewolken/index.php?title=Badges/SQL-0\r\n\r\nAls bewijs lever je de link in naar je wikipagina waar je de uitwerkingen neergezet hebt.\r\n\r\n&nbsp;\r\n\r\n&nbsp;',
+  //        image: 'https://credlyapp.s3.amazonaws.com/badges/8ad76304ff4d51edb2c6a380c3175f94.png' },
+  //      { title: 'HTML-0',
+  //        description: 'De inhoud van de badge en de ondersteunende module kan je vinden in de wiki.\r\n\r\nhttp://infvo.com/ictindewolken/index.php?title=Badges/HTML-0\r\n\r\nAls bewijs lever je de link naar je gemaakte website in.',
+  //        image: '/offline_dev/fef4b7203b1b5e5f05e192ced26118c9.png' },
+  //      { title: 'Linux-0',
+  //        description: 'Deze badge geeft aan dat je een aantal basisprincipes van Linux beheerst, zoals je deze als gebruiker tegenkomt.\r\n\r\nVoor de opdrachten, zie: http://infvo.com/basis/index.php?title=Modules/Linux-0',
+  //        image: 'https://credlyapp.s3.amazonaws.com/badges/979a57791a2e2827983f80aebfd24041.png' },
+  //      { title: 'Credly Member',
+  //        description: 'Credly is the universal way to recognize, store, and share evidence of lifelong achievement in any setting. This credit recognizes those who have joined and are now active members of the growing Credly community.',
+  //        image: '/offline_dev/0c883ab7a89327d278050bb90a1daf5a.png' } ] }
   // })
-
-  // credly.request(`/members/${req.params.id}`)
-  //   .then(data => {
-  //     user.name = data.data.display_name
-  //     return credly.request(`/members/${data.data.id}/badges`)
-  //   })
-  //   .then(data => {
-  //     console.timeEnd('credly api request')
-  //
-  //     user.badges = data.data.map(badge => {
-  //       return {
-  //         title: badge.title,
-  //         description: badge.description,
-  //         image: badge.image
-  //       }
-  //     })
-  //
-  //     res.render('index',{user: user})
-  //   })
-
-  res.render('badges',{user:
-  { name: 'Noah Loomans',
-    badges:
-     [ { title: 'SQL-0',
-         description: 'De inhoud van de badge vind je onder\r\n\r\nhttp://infvo.com/ictindewolken/index.php?title=Badges/SQL-0\r\n\r\nAls bewijs lever je de link in naar je wikipagina waar je de uitwerkingen neergezet hebt.\r\n\r\n&nbsp;\r\n\r\n&nbsp;',
-         image: 'https://credlyapp.s3.amazonaws.com/badges/8ad76304ff4d51edb2c6a380c3175f94.png' },
-       { title: 'HTML-0',
-         description: 'De inhoud van de badge en de ondersteunende module kan je vinden in de wiki.\r\n\r\nhttp://infvo.com/ictindewolken/index.php?title=Badges/HTML-0\r\n\r\nAls bewijs lever je de link naar je gemaakte website in.',
-         image: '/offline_dev/fef4b7203b1b5e5f05e192ced26118c9.png' },
-       { title: 'Linux-0',
-         description: 'Deze badge geeft aan dat je een aantal basisprincipes van Linux beheerst, zoals je deze als gebruiker tegenkomt.\r\n\r\nVoor de opdrachten, zie: http://infvo.com/basis/index.php?title=Modules/Linux-0',
-         image: 'https://credlyapp.s3.amazonaws.com/badges/979a57791a2e2827983f80aebfd24041.png' },
-       { title: 'Credly Member',
-         description: 'Credly is the universal way to recognize, store, and share evidence of lifelong achievement in any setting. This credit recognizes those who have joined and are now active members of the growing Credly community.',
-         image: '/offline_dev/0c883ab7a89327d278050bb90a1daf5a.png' } ] }
-  })
-
 })
 
 module.exports = router
