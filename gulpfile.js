7a884141ba85d6724a5c82e61097341997de9b69
Noah Loomans <noahloomans@gmail.com>
2016-08-22 13:09:18 +0200

    Made the life of a developer easier

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,46 @@
-'use strict';
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const spawn = require('child_process').spawn
 
-const gulp = require('gulp');
-const sass = require('gulp-sass');
+let node
 
-gulp.task('default', ['sass','sass:watch']);
+const paths = {
+  javascript: {
+    server: ['./bin/www', './app.js', './lib/**/*.js', './routes/**/*.js'],
+    client: ['./public/scripts/**/*.js']
+  },
+  sass: ['./public/stylesheets/**/*.sass']
+}
+
+function killNode () {
+  return new Promise((resolve, reject) => {
+    if (node) {
+      node.on('close', resolve)
+      node.kill('SIGINT')
+    } else {
+      resolve()
+    }
+  })
+}
+
+gulp.task('server', function () {
+  return killNode().then(function () {
+    node = spawn('node', [`./bin/www`], {stdio: 'inherit'})
+  })
+})
+
+gulp.task('server:watch', function () {
+  return gulp.watch(paths.javascript.server, ['server'])
+})
 
 gulp.task('sass', function () {
   return gulp.src('./public/stylesheets/**/*.sass')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./public/stylesheets'));
-});
+    .pipe(gulp.dest('./public/stylesheets'))
+})
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./public/stylesheets/**/*.sass', ['sass']);
-});
+  return gulp.watch(paths.sass, ['sass'])
+})
+
+gulp.task('default', ['sass', 'sass:watch', 'server', 'server:watch'])
