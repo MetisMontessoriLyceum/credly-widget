d42a8144989dcd839a9e92478fa80d03dab04667
Noah Loomans <noahloomans@gmail.com>
2016-11-17 12:47:49 +0100

    Added table ranking

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 
 const user = require('./routes/user')
-const ranking = require('./routes/ranking')
+const table = require('./routes/table')
 
 const app = express()
 
@@ -20,7 +20,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/user', user)
-app.use('/ranking', ranking)
+app.use('/table', table)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
