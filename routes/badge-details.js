35bd1ca80077e0a174f754004eb2513ed6e0eb0c
Noah Loomans <noahloomans@gmail.com>
2017-01-11 12:48:03 +0100

    add badge-details view

---
+++ routes/badge-details.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const router = express.Router()
+const credly = require('../lib/credly')
+const ty = require('then-yield').using(Promise.cast)
+
+router.get('/:issuerId/:badgeId', ty.async(function* (req, res, err) {
+  const issuerId = yield credly.resolveUserId(req.params.issuerId)
+  const badgeId = req.params.badgeId
+
+  // example issuerId: 2221531
+  // example badgeId: 99999 // yes, we actualy got this id by pure luck.
+
+  const badgesGiven =
+      (yield credly.request(`/members/${issuerId}/badges/given?per_page=100`))
+      .filter(badge => badge.badge_id.toString() === badgeId)
+
+  console.log(badgesGiven.length)
+
+  if (badgesGiven.length === 0) {
+    res.render('badge-details', { badge: { name: 'Unknown' } })
+    return
+  }
+
+  const badge = {
+    name: badgesGiven[0].title,
+    given: badgesGiven
+  }
+
+  res.render('badge-details', { badge })
+}))
+
+module.exports = router
