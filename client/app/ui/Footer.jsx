6ae105c2f9f1c18017c566d4447fb82459be5285
Noah Loomans <noahloomans@gmail.com>
2017-07-16 21:55:05 +0200

    Add proper footer

--- client/app/ui/Footer.jsx
+++ client/app/ui/Footer.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
-import Badge from './Badge.jsx'
+import CodeIcon from 'react-icons/lib/fa/code'
+import HeartIcon from 'react-icons/lib/fa/heart'
 
 export default class User extends React.Component {
   render () {
     return (
-      <div>
-        &lt;&gt; with &lt;3 by Noah Loomans
+      <div style={{
+        display: 'flex',
+        alignItems: 'center',
+        color: '#424242',
+        marginTop: 8
+      }}>
+        <div style={{ flexGrow: 1 }} />
+        <CodeIcon style={iconStyle} /> with <HeartIcon style={iconStyle} /> by Noah Loomans
       </div>
     )
   }
+}
+
+const iconStyle = {
+  paddingLeft: 8,
+  paddingRight: 8
 }
